b836ebd9ff87d5d045b21bf85a3737dbbb71bcaf
SamahOud <you@example.com>
2023-11-24 14:07:43 +0200

    .

--- ProjectsJS/JS-3-ScreenBuilder/js.js
+++ ProjectsJS/JS-3-ScreenBuilder/js.js
@@ -1,8 +1,8 @@
 const body = document.querySelector("body"),
-    sidebar = body.querySelector(".sidebar"),
-    toggle = body.querySelector(".toggle"),
-    modeSwitch = body.querySelector(".toggle-switch"),
-    modeText = body.querySelector(".mode-text");
+      sidebar = body.querySelector(".sidebar"),
+      toggle = body.querySelector(".toggle"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
 
 toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
